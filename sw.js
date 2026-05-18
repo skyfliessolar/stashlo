c9108ce5f282f0d6125ae140eef1ca581e7fc5ef
Stashlo Deploy <deploy@stashlo.app>
2026-05-18 19:18:17 +0000

    v3.1: Fix service worker cache, force rebuild

--- sw.js
+++ sw.js
@@ -1,1 +1,14 @@
-// test
+// Stashlo SW v3 - cache bust
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+});
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.map(function(k) { return caches.delete(k); }));
+    }).then(function() { return self.clients.claim(); })
+  );
+});
+self.addEventListener('fetch', function(e) {
+  e.respondWith(fetch(e.request));
+});
